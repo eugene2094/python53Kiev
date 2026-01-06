582409184b090970a3ebb93b54a74634985fa83b
evgen <70202369+eugene2094@users.noreply.github.com>
2026-01-06 21:52:56 +0200

    add new file about polimorfism

--- сw_oop_abc_meta.py
+++ сw_oop_abc_meta.py
@@ -55,3 +55,71 @@ class CashPayment(PaymentSystem):
 
     def pay(self, amount):
         print(f"Pay with cash {amount}")
+
+
+class TestClass:
+    pass
+
+
+obj = TestClass()
+
+print(type(type(obj)))
+
+
+def method1(self):
+    print("Hello")
+
+
+NewClass = type('NewClass', (TestClass,), {'attrr1': 1, 'method1': method1})
+
+obj1 = NewClass()
+print(obj1)
+print(type(obj1))
+obj1.method1()
+
+
+class MyMetaClass(type):
+    def __new__(cls, name, bases, dict):
+        print("Type class created - ", cls)
+        print("name of class", name)
+        print("bases list", bases)
+        print("list attr", dict)
+        return type.__new__(cls, name, bases, dict)
+
+
+class MyClass1(metaclass=MyMetaClass):
+    attr1 = 1
+
+
+print(type(MyMetaClass))
+print(type(MyClass1))
+
+
+class TestAttrMeta(type):
+    def __new__(cls, name, bases, dict):
+        if 'id' not in dict.keys():
+            print("No id attr in class ", name)
+        else:
+            methods = {key: value for key, value in dict.items() if callable(value)}
+            if len(methods) > 2:
+                print("More than 2 methods !!")
+            else:
+                print("Class is creating !", name)
+                return type.__new__(cls, name, bases, dict)
+
+
+class User(metaclass=TestAttrMeta):
+    atrr = 3
+    id = 1
+
+    def m1(self):
+        pass
+
+    def m2(self):
+        pass
+
+    def m3(self):
+        pass
+
+user1 = User()
+print(user1.id)
