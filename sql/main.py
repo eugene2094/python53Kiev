5a510e1bb42ccab9046ebc5ba6730cfb05e40033
evgen <70202369+eugene2094@users.noreply.github.com>
2026-02-10 21:56:47 +0200

    update sql main file

--- sql/main.py
+++ sql/main.py
@@ -3,7 +3,7 @@ import random
 
 
 def main():
-    conn = sqlite3.connect('db3.sqlite')
+    conn = sqlite3.connect('db4.sqlite')
     conn.execute('PRAGMA foreign_keys=ON;')
     cursor = conn.cursor()
 
@@ -25,8 +25,20 @@ def main():
             order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
             FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE            
        );   
-           
+       CREATE TABLE IF NOT EXISTS users(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       name TEXT NOT NULL,
+       email TEXT NOT NULL UNIQUE
+      ) ;
     """)
+
+    # ALTER TABLE orders ADD COLUMN user_id
+    # INTEGER REFERENCES users(id) ON DELETE CASCADE;
+
+    # INSERT INTO users(name,email) VALUES
+    # ('Ilon','ilon@gmail.com'),
+    # ('Maria','maria@gmail.com'),
+    # ('Borus','Borus@gmail.com');
 
     # cursor.executemany("INSERT INTO products (name,price,stock) VALUES (?,?,?)", [
     #     ('iphone 5', 600, 1),
@@ -34,15 +46,15 @@ def main():
     #     ('iphone 7', 800, 10),
     #     ('samsung galaxy S23', 900, 10)
     # ])
-    model_id = random.randint(1, 4)
-    cursor.execute(" SELECT price FROM products WHERE id = ?", (model_id,))
-    product_price = cursor.fetchone()[0]
-
-    quantity = random.randint(1, 10)
-    total_price = product_price * quantity
-
-    cursor.execute("INSERT INTO orders (product_id,quantity,total_price) VALUES (?,?,?)",
-                   (model_id, quantity, total_price))
+    # model_id = random.randint(1, 4)
+    # cursor.execute(" SELECT price FROM products WHERE id = ?", (model_id,))
+    # product_price = cursor.fetchone()[0]
+    #
+    # quantity = random.randint(1, 10)
+    # total_price = product_price * quantity
+    #
+    # cursor.execute("INSERT INTO orders (product_id,quantity,total_price,user_id) VALUES (?,?,?,?)",
+    #                (model_id, quantity, total_price,random.randint(1,3)))
 
     # cursor.execute("SELECT * FROM products")
     # cursor.execute("SELECT name,price FROM products")
@@ -60,18 +72,49 @@ def main():
     # cursor.execute("UPDATE products SET stock = ? WHERE id = ?", (8, 2))
     # cursor.execute("SELECT * FROM orders;")
 
+    # cursor.execute("""
+    #    SELECT products.name, orders.quantity,orders.total_price
+    #    FROM orders
+    #    JOIN products ON orders.product_id = products.id;
+    # """)
+
     cursor.execute("""
-       SELECT products.name, orders.quantity,orders.total_price
+       SELECT orders.id, users.name AS user_name, products.name AS product_name, orders.quantity,orders.total_price,
+       orders.order_date 
        FROM orders
-       JOIN products ON orders.product_id = products.id;    
+       JOIN users ON orders.user_id = users.id
+       JOIN products ON orders.product_id = products.id;
     """)
 
+    cursor.execute("""
+           SELECT users.name AS user_name, products.name AS product_name, orders.quantity,orders.total_price 
+           FROM orders
+           JOIN users ON orders.user_id = users.id
+           JOIN products ON orders.product_id = products.id
+           WHERE users.name = "Maria"
+        """)
 
+    cursor.execute("""
+               SELECT users.name AS user_name, products.name AS product_name, orders.quantity,orders.total_price 
+               FROM orders
+               JOIN users ON orders.user_id = users.id
+               JOIN products ON orders.product_id = products.id
+               ORDER BY orders.total_price DESC
+            """)
+
+    cursor.execute("""
+               SELECT users.name, SUM(orders.total_price) AS total_spent 
+               FROM orders
+               JOIN users ON orders.user_id = users.id
+               GROUP BY users.id
+               ORDER BY total_spent DESC
+            """)
 
     for row in cursor.fetchall():
         print(row)
 
     conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
